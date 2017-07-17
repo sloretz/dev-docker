f1f70ac0eefb28f4d728d7ed1b112f7ad5ca459b
Shane Loretz <sloretz@osrfoundation.org>
2017-07-17 08:10:58 -0700

    Added stuff for ros2

---
+++ run_ws.bash
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: $0 <docker image> <dir with workspace>"
+    exit 1
+fi
+
+IMG=$(basename $1)
+WS_DIR=$2
+
+if [ ! -d $WS_DIR/src ]
+then
+  echo "Workspace directory does not have a folder called 'src'"
+  exit 1
+fi
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+DOCKER_OPTS=
+
+VIMRC=~/.vimrc
+if [ -f $VIMRC ]
+then
+  DOCKER_OPTS="$DOCKER_OPTS -v $VIMRC:/home/developer/.vimrc:ro"
+fi
+
+SPNAV_SOCK=/var/run/spnav.sock
+if [ -f $SPNAV_SOCK ]
+then
+  DOCKER_OPTS="$DOCKER_OPTS -v /var/run/spnav.sock:/var/run/spnav.sock"
+fi
+
+sudo nvidia-docker run -it \
+  -e DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XAUTHORITY=$XAUTH \
+  -v "$XAUTH:$XAUTH" \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+  -v "/etc/localtime:/etc/localtime:ro" \
+  --rm=true \
+  --security-opt seccomp=unconfined \
+  -v "$WS_DIR:/workspace" \
+  $DOCKER_OPTS \
+  $IMG
