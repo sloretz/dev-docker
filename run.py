e3a98a9d8b37e631b5c285dcf3c9f70899c7df02
Shane Loretz <sloretz@osrfoundation.org>
2019-01-09 15:20:40 -0800

    Allow running images of dockerhub

--- run.py
+++ run.py
@@ -7,31 +7,33 @@ import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('image_folder', metavar='IMAGE', type=str,
-                        help='Path to folder containing a Dockerfile')
+    parser.add_argument('image', metavar='IMAGE', type=str,
+                        help='Name of an image or a path to a folder containing a Dockerfile')
     parser.add_argument('mounts', metavar='PATH', type=str,
                         nargs='*', help='Paths to mount in the container')
     parser.add_argument('--no-cache', action='store_true',
                         help='Ignore cache when building Docker images')
     args = parser.parse_args()
 
-    if not os.path.isdir(args.image_folder):
-        raise OSError('"{}" is not a directory'.format(args.image_folder))
+    if os.path.isdir(args.image):
+        # If the image is a path, then try building a docker image there
 
-    image_name = os.path.basename(args.image_folder)
-    if '' == image_name:
-        image_name = image_name = os.path.basename(args.image_folder[:-1])
+        image_name = os.path.basename(args.image)
+        if '' == image_name:
+            image_name = image_name = os.path.basename(args.image[:-1])
 
-    # Build the docker image
-    docker_cmd = ['docker', 'build', '--force-rm']
-    docker_cmd += ['-t', image_name]
-    if args.no_cache:
-        docker_cmd.append('--no-cache')
-    docker_cmd.append(args.image_folder)
-    exit_code = os.spawnvp(os.P_WAIT, 'docker', docker_cmd)
-    if 0 != exit_code:
-        sys.stderr.write('Failed to build docker image\n')
-        sys.exit(exit_code)
+        docker_cmd = ['docker', 'build', '--force-rm']
+        docker_cmd += ['-t', image_name]
+        if args.no_cache:
+            docker_cmd.append('--no-cache')
+        docker_cmd.append(args.image)
+        exit_code = os.spawnvp(os.P_WAIT, 'docker', docker_cmd)
+        if 0 != exit_code:
+            sys.stderr.write('Failed to build docker image\n')
+            sys.exit(exit_code)
+    else:
+        # Not a path? Must be an image from docker hub
+        image_name = args.image
 
     # build command for rocker 
     rocker_cmd = ['rocker', '--execute', '--nvidia', '--user']
